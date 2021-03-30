1d7cfa992d9df3e6776c6a5ecb1cd40bbf7d1dd9
Brandon <defoe.brand@gmail.com>
2021-03-30 12:20:16 +0300

    Establish domain redirect

--- app/controllers/app_controller.rb
+++ app/controllers/app_controller.rb
@@ -1,4 +1,15 @@
 # frozen_string_literal: true
 
 class AppController < ApplicationController
+  before_action :decode_url, only: [:show]
+
+  def show
+    redirect_to @url
+  end
+
+  private
+
+  def decode_url
+    @url = params[:url] == 'asdf' ? 'http://www.google.com' : root_path
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
+  get '/:url', to: 'app#show'
   root 'app#index'
 end
