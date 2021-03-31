a89e54ee811193dc286eec2cd5e444973a499086
Brandon <defoe.brand@gmail.com>
2021-03-31 18:07:36 +0300

    Adjust view tests

--- spec/views/app/index_spec.rb
+++ spec/views/app/index_spec.rb
@@ -3,39 +3,28 @@
 require 'rails_helper'
 
 describe 'the shortening process', type: :feature do
-  it 'returns shortened url with user input' do
+  before do
     visit root_path
     sleep(2)
-    fill_in 'input', with: 'www.google.com'
-    sleep(2)
-    click_button 'The Big Short'
-    sleep(2)
-    expect(page).to have_content 'localhost:3000/www.google.com'
   end
-end
-
-describe 'the shortening process', type: :feature do
-  it 'redirects user when visiting shortened url' do
-    visit root_path
-    sleep(2)
-    fill_in 'input', with: 'fdsa'
-    sleep(2)
-    click_button 'The Big Short'
-    sleep(2)
-    click_link 'localhost:3000/fdsa'
-    sleep(2)
+  it `displays "Honey, let's shrink the urls!"` do
     expect(page).to have_content "Honey, let's shrink the urls!"
   end
-
-  it 'redirects user to google if url is asdf' do
-    visit root_path
+  it 'does not display a shortened link without user input' do
+    expect(page).not_to have_content 'localhost:3000/'
+  end
+  it 'replaces message with shortened url' do
+    fill_in 'input', with: 'www.google.com'
     sleep(2)
-    fill_in 'input', with: 'asdf'
+    click_button 'Get Shorty'
     sleep(2)
-    click_button 'The Big Short'
+    expect(page).not_to have_content "Honey, let's shrink the urls!"
+  end
+  it 'replaces message with shortened url' do
+    fill_in 'input', with: 'www.google.com'
     sleep(2)
-    click_link 'localhost:3000/asdf'
+    click_button 'Get Shorty'
     sleep(2)
-    expect(page).to have_content 'Gmail'
+    expect(page).to have_content 'localhost:3000/'
   end
 end
