a60c3fa3c745d17f88ecbd60fe90981183f7d477
Brandon <defoe.brand@gmail.com>
2021-03-31 18:17:12 +0300

    Stage app controller tests - pending test seeds

--- spec/requests/app_spec.rb
+++ spec/requests/app_spec.rb
@@ -7,16 +7,16 @@ describe 'URL Shortener', type: :system do
     it 'shows the right content' do
       visit root_path
       sleep(2)
-      expect(page).to have_content('Get Shorty')
+      expect(page).to have_content('The Big Short')
     end
   end
   describe 'show page' do
-    it 'redirects to root_path if url != asdf' do
+    it 'redirects to root_path if short key is not registered' do
       visit '/fdsa'
       sleep(2)
-      expect(page).to have_content('Get Shorty')
+      expect(page).to have_content('The Big Short')
     end
-    it 'redirects to google if url == asdf' do
+    it 'redirects to google.com if short key matches stored google.com key' do
       visit '/asdf'
       sleep(2)
       expect(page).to have_content('Gmail')
